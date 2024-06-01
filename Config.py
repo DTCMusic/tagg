edf3ebb1225a83b8541cf8d3a4af0e9444246eeb
DTCMusic <89629350+DTCMusic@users.noreply.github.com>
2024-06-01 19:50:52 +0400

    Update Config.py

--- Config.py
+++ Config.py
@@ -4,13 +4,13 @@ import os
 class Config():
     # Bu dəyərləri my.telegram.org saytından əldə edin
     #>>> https://my.telegram.org
-    API_ID = int(os.environ.get("API_ID","16157584"))
-    API_HASH = os.environ.get("API_HASH","2167d4e6007a79eed91d084bf5b8966a")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN","5337183930:")
-    BOT_USERNAME = os.environ.get("BOT_USERNAME","dejavy")
-    BOT_NAME = os.environ.get("BOT_NAME","jhkoijl")
-    BOT_ID = int(os.environ.get("BOT_ID","234567890-"))
-    SUDO_USERS = os.environ.get("SUDO_USERS","rseyutiydou").split()
+    API_ID = int(os.environ.get("API_ID","16089646"))
+    API_HASH = os.environ.get("API_HASH","7ce9ffba92ce7e56ca7d5e6f73201002")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN","7493261373:AAFwQVBj3IOu557mqGFNnOrcG3YnnNOxsCA:")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME","NezrinChatRobot")
+    BOT_NAME = os.environ.get("BOT_NAME","nezrin")
+    BOT_ID = int(os.environ.get("BOT_ID","7493261373"))
+    SUDO_USERS = os.environ.get("SUDO_USERS","thagiyev").split()
     SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT","ioj")
-    OWNER_ID = int(os.environ.get("OWNER_ID","43567890"))
+    OWNER_ID = int(os.environ.get("OWNER_ID","6634423600"))
     OWNER_USERNAME = os.environ.get("OWNER_USERNAME","uijio;kl,")
